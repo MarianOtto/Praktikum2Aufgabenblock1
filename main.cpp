51e6b16e2869bd22257aaf35dd2f0aedd82c9743
Marian Otto <marian.otto@gmx.net>
2023-10-12 16:18:04 +0200

    fix vAufgabe 3 hinzugefügt \n Aufgabenblock 1 fertig

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
  *      Author: marian
  */
 
-#define TEST 0;
+#define TEST 0
 
 #include <iostream>
 #include <memory>
@@ -36,10 +36,16 @@ std::ostream& operator<<(std::ostream& ostr, const Fahrzeug& Fahrzeug)
 
 void vAufgabe_1()
 {
+	std::cout << "\n\n Aufgabe 1 \n\n";
+
 	Fahrzeug Auto("Auto");
 	Fahrzeug Bus;
 	Fahrzeug* Zug = new Fahrzeug("Zug");
+	std::cout << "Test vAusgeben() und vKopf(): " << std::endl;
+	Zug->vKopf();
+	std::cout << std::endl;
 	Zug->vAusgeben();
+	std::cout << std::endl;
 	std::unique_ptr<Fahrzeug> unique1 = std::make_unique<Fahrzeug>("Unique1");
 	std::unique_ptr<Fahrzeug> unique2 = std::make_unique<Fahrzeug>("Unique2");
 	std::shared_ptr<Fahrzeug> shared1 = std::make_shared<Fahrzeug>("Shared1");
@@ -55,21 +61,14 @@ void vAufgabe_1()
 	vector_shared.push_back(shared1);
 	vector_shared.push_back(shared2);
 	vector_shared.push_back(std::move(shared2_1));
-	std::cout << shared2.use_count() << " Count after vector" << std::endl;
+	std::cout << "Count after vector " << shared2.use_count() << std::endl;
 	vector_shared.clear();
 }
 
-void vRunTests()
-{
-
-}
-
-void vTestdTanken()
-{
-	double dMenge = static_cast <double> (rand()) / static_cast<double>(RAND_MAX / 100);
-}
 void vAufgabe_1a()
 {
+	std::cout << "\n\n Aufgabe 1a \n\n";
+
 	std::unique_ptr<Fahrzeug> Auto = std::make_unique<Fahrzeug>("Auto", 130);
 	std::unique_ptr<Fahrzeug> Bus = std::make_unique<Fahrzeug>("Bus", 90);
 	std::unique_ptr<Fahrzeug> Fahrrad = std::make_unique<Fahrzeug>("Fahrrad",25);
@@ -79,6 +78,7 @@ void vAufgabe_1a()
 	vector_Fahrzeuge.push_back(std::move(Fahrrad));
 	for(int i = 0; i < 3; i++)
 	{
+		std::cout << std::endl << "Simulationsschritt: " << i << std::endl;
 		std::cout << std::endl;
 		vector_Fahrzeuge.at(1)->vKopf();
 		std::cout << std::endl;
@@ -96,6 +96,8 @@ void vAufgabe_1a()
 
 void vAufgabe_2()
 {
+	std::cout << "\n\n Aufgabe 2 \n\n";
+
 	int iCountPKW;
 	int iCountFahrrad;
 	std::cout << "Bitte Anzahl an Autos eingeben: ";
@@ -110,7 +112,6 @@ void vAufgabe_2()
 		for(int i = 1; i <= iCountPKW; i++)
 		{
 			std::string sName = "PKW" + std::to_string(i);
-			std::cout << sName;
 			std::unique_ptr<PKW> PKWs = std::make_unique<PKW>(sName, (double) (rand() % 5 * 20 + 20), (double) (rand() % 5 * 10 + 25), (double) (rand() % 5) + 1);
 			vector_Fahrzeug.push_back(std::move(PKWs));
 		}
@@ -135,6 +136,7 @@ void vAufgabe_2()
 		double dDeltaT = rand() % 210;
 		dDeltaT /= 60;
 		dGlobaleZeit += dDeltaT;
+		std::cout << std::endl <<  std::endl << "Simulationsschritt: " << i << std::endl << std::endl;
 		vector_Fahrzeug.at(0)->vKopf();
 		for(auto &Fahrzeug : vector_Fahrzeug)
 		{
@@ -143,7 +145,84 @@ void vAufgabe_2()
 			Fahrzeug->vAusgeben();
 		}
 	}
+}
 
+void vAufgabe_3()
+{
+	std::cout << "\n\n Aufgabe 3 \n\n";
+
+	PKW PKW1("PKW1", 100, 50, 4.5);
+	PKW PKW2("PKW2");
+	Fahrzeug Fahrzeug("Fahrzeug", 100);
+	Fahrrad Fahrrad("Fahrrad", 20);
+
+	std::cout << "Ausgabe mit Operator Overload :" << std::endl << std::endl;
+	PKW1.vKopf();
+	std::cout << std::endl << PKW1 << std::endl << Fahrrad << std::endl << std::endl;
+
+	std::cout << "Test Vergleichsopator:" << std::endl << std::endl;
+	std::cout << "Vor Simulation: " << std::endl;
+	PKW1.vKopf();
+	std::cout << std::endl << PKW1 << std::endl << PKW2 << std::endl;
+	if(PKW2<PKW1)
+	{
+		std::cout << PKW1.getsName() << " ist weiter gefahren";
+	}
+	else
+	{
+		std::cout << PKW2.getsName() << " ist weiter gefahren";
+	}
+	std::cout << std::endl << std::endl;
+
+	dGlobaleZeit += 2;
+	PKW1.vSimulieren();
+	PKW2.vSimulieren();
+
+	std::cout << "Nach Simulation: " << std::endl;
+	PKW1.vKopf();
+	std::cout << std::endl << PKW1 << std::endl << PKW2 << std::endl;
+	if(PKW2<PKW1)
+	{
+		std::cout << PKW1.getsName() << " ist weiter gefahren";
+	}
+	else
+	{
+		std::cout << PKW2.getsName() << " ist weiter gefahren";
+	}
+	std::cout << std::endl;
+
+	std::cout << "Teste Zuweisung:" << std::endl << std::endl;
+
+	std::cout << "Fahrzeug = Fahrrad" << std::endl;
+	std::cout << "Zuvor: \n\n";
+	PKW1.vKopf();
+	std::cout << std::endl << Fahrzeug << std::endl << Fahrrad << std::endl << std::endl;
+	Fahrzeug = Fahrrad;
+	std::cout << "Danach: \n\n" << Fahrzeug << std::endl << Fahrrad << std::endl << std::endl;
+
+	std::cout << "Fahrzeug/PKW" << std::endl;
+	std::cout << "Fahrzeug = PKW1" << std::endl;
+	std::cout << "Zuvor: \n\n";
+	PKW1.vKopf();
+	std::cout << std::endl << Fahrzeug << std::endl << PKW1 << std::endl << std::endl;
+	Fahrzeug = PKW1;
+	std::cout << "Danach: \n\n" << Fahrzeug << std::endl << PKW1 << std::endl << std::endl;
+
+	std::cout << "PKW/Fahrzeug" << std::endl;
+	std::cout << "PKW2 = Fahrzeug" << std::endl;
+	std::cout << "Zuvor: \n\n";
+	PKW1.vKopf();
+	std::cout << std::endl << PKW2 << std::endl << Fahrzeug << std::endl << std::endl;
+	Fahrzeug = Fahrrad;
+	std::cout << "Danach: \n\n" << PKW2 << std::endl << Fahrzeug << std::endl << std::endl;
+
+	std::cout << "PKW/PKW" << std::endl;
+	std::cout << "PKW1 = PKW2" << std::endl;
+	std::cout << "Zuvor: \n\n";
+	PKW1.vKopf();
+	std::cout << std::endl << PKW1 << std::endl << PKW2 << std::endl << std::endl;
+	Fahrzeug = Fahrrad;
+	std::cout << "Danach: \n\n" << PKW1 << std::endl << PKW2 << std::endl << std::endl;
 
 }
 
@@ -153,11 +232,9 @@ int main(void)
 
 #endif
 	std::srand(std::time(NULL));//
-	//vAufgabe_1();
-	//vAufgabe_1a();
-	//vAufgabe_2();
-	PKW PKW("Auto", 30, 30 ,30);
-	PKW.vKopf();
-	std::cout << std::endl << PKW;
+	vAufgabe_1();
+	vAufgabe_1a();
+	vAufgabe_2();
+	vAufgabe_3();
 }
 
